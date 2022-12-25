e14388c8198e47ab1b0d46a6605c17a3cb7a4d30
hashi-02 <hassie0209@gmail.com>
2022-12-26 06:31:29 +0900

    詳細情報ページ作成

---
+++ pages/restaurants/[post].js
@@ -0,0 +1,27 @@
+export default function restaurantDetailInfo({ restaurantDetailInfo }) {
+  return (
+    <div>
+      <h1>
+        restaurantDetailInfo(投稿){restaurantDetailInfo.results.shop[0].id}
+      </h1>
+      <h2>{restaurantDetailInfo.results.shop[0].name}</h2>
+      <p>{restaurantDetailInfo.results.shop[0].mobile_access}</p>
+      <p>{restaurantDetailInfo.results.shop[0].open}</p>
+      <p>{restaurantDetailInfo.results.shop[0].photo.pc.l}</p>
+    </div>
+  );
+}
+
+export async function getServerSideProps({ params }) {
+  const id = params.post;
+  const apiBaseUrl = process.env.API_URL_ROOT;
+  const res = await fetch(`${apiBaseUrl}&id=${id}`);
+  const restaurantDetailInfo = await res.json();
+  console.log(restaurantDetailInfo.results.shop[0]);
+  if (!Object.keys(restaurantDetailInfo).length) {
+    return {
+      notFound: true,
+    };
+  }
+  return { props: { restaurantDetailInfo } };
+}
