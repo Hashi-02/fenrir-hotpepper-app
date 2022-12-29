dea8b98b765c6d0f0c6508ae298b1fa066819f99
hashi-02 <hassie0209@gmail.com>
2022-12-29 22:12:49 +0900

    titileなどの設定

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,42 @@
 import '../styles/globals.css';
 import Layout from '../organism/layout';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Head>
+        <title>GPSグルメサーチ</title>
+        <meta property="og:title" content="GPS gourmet Research Appliation" />
+        <meta
+          property="og:description"
+          content="This isGPS gourmet Research Appliation"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicons//apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicons//favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
