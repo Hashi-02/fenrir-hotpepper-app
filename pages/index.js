24c8c9bb3af6a2e9c4542f8451331a80a51e08e6
hashi-02 <hassie0209@gmail.com>
2022-12-29 22:37:50 +0900

    homeコンポーネントスタイル適応

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
+import style from './styles/index.module.scss';
 export default function Home() {
   return (
-    <div>
+    <div className={style.wrapper}>
       <h1>周辺のレストラン</h1>
     </div>
   );
